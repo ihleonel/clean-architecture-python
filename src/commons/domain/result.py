ebea598a02186155ff4edb35eef56151fc2ca7ea
Ibarra Hector Leonel <ibarra.h.leonel@gmail.com>
2026-03-05 18:05:05 -0300

    feat: enhance user validation to return detailed error messages

--- src/commons/domain/result.py
+++ src/commons/domain/result.py
@@ -3,13 +3,14 @@ from typing import TypeVar, Union, Generic
 
 
 T = TypeVar("T")
+E = TypeVar("E")
 
 @dataclass(frozen=True)
-class Error:
-    errors: dict[str, list[str]]
+class Error(Generic[E]):
+    errors: list[E]
 
 @dataclass(frozen=True)
 class Success(Generic[T]):
     value: T
 
-Result = Union[Success[T], Error]
+Result = Union[Success[T], Error[E]]
--- src/users/application/user_validate_creation.py
+++ src/users/application/user_validate_creation.py
@@ -1,47 +1,61 @@
 from src.users.domain.user_repository import UserRepository
-from src.commons.domain.result import Result, Error, Success
+from src.commons.domain.result import Result as Validation, Error, Success
 
 
 class UserValidateCreation:
     def __init__(self, user_repository: UserRepository):
         self.user_repository = user_repository
 
-    def validate(self, name: str, email: str, password: str) -> Result[None]:
+    def validate(self, name: str, email: str, password: str) -> Validation[None, dict[str, list[str]]]:
         errors: dict[str, list[str]] = {}
 
         name_result = self.validate_name(name)
-        if isinstance(name_result, Error):
-            errors.update(name_result.errors)
+        if name_result:
+            errors.update(name_result)
 
         email_result = self.validate_email(email)
-        if isinstance(email_result, Error):
-            errors.update(email_result.errors)
+        if email_result:
+            errors.update(email_result)
 
         password_result = self.validate_password(password)
-        if isinstance(password_result, Error):
-            errors.update(password_result.errors)
+        if password_result:
+            errors.update(password_result)
 
         if errors:
-            return Error(errors)
+            return Error([errors])
         return Success(None)
 
-    def validate_name(self, name: str) -> Result[None]:
+    def validate_name(self, name: str) -> dict[str, list[str]] | None:
+        errors = dict[str, list[str]]()
         if not name:
-            return Error({"name": ["Name is required"]})
-        return Success(None)
+            errors["name"] = ["Name is required"]
+        elif len(name) < 3:
+            errors["name"] = ["Name must be at least 3 characters long"]
+        if errors:
+            return errors
+        return None
 
-    def validate_email(self, email: str) -> Result[None]:
+    def validate_email(self, email: str) -> dict[str, list[str]] | None:
+        errors = dict[str, list[str]]()
         if not email:
-            return Error({"email": ["Email is required"]})
+            errors["email"] = ["Email is required"]
         elif self.user_repository.email_already_exists(email):
-            return Error({"email": ["Email already exists"]})
-        return Success(None)
+            errors["email"] = ["Email already exists"]
+        if errors:
+            return errors
+        return None
 
-    def validate_password(self, password: str) -> Result[None]:
+    def validate_password(self, password: str) -> dict[str, list[str]] | None:
+        errors = dict[str, list[str]]()
         if not password:
-            return Error({"password": ["Password is required"]})
-        elif len(password) < 8:
-            return Error({"password": ["Password must be at least 8 characters long"]})
-        elif not any(char.isdigit() for char in password):
-            return Error({"password": ["Password must contain at least one digit"]})
-        return Success(None)
+            errors["password"] = ["Password is required"]
+
+        if len(password) < 8:
+            errors["password"] = ["Password must be at least 8 characters long"]
+
+        if not any(char.isdigit() for char in password):
+            errors["password"] = ["Password must contain at least one digit"]
+
+        if errors:
+            return errors
+        return None
